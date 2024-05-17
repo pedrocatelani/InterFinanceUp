547210a674f1adfc9b04766f759d5ffc980797a4
Tales Brandt <tales.bnascimento@gmail.com>
2024-05-17 14:53:52 -0300

    Login e Signin

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews(); //suportar MVC
-
 var app = builder.Build();
-
+app.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
 app.Run();
