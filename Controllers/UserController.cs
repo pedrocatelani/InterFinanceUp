a669ff57a7b7ad942a6980340efbd912338296aa
Tales Brandt <tales.bnascimento@gmail.com>
2024-05-17 14:53:52 -0300

    Login e Signin

---
+++ Controllers/UserController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class UserController: Controller {
+    public ActionResult SignIn(){
+        return View();
+    }
+    public ActionResult LogIn(){
+        return View();
+    }
+}
